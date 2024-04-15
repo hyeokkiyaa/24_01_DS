e0b2fe7cee27741722e90343a4ba28c769c810b6
hyeokkiyaa <khm38607574@gmail.com>
2024-04-15 23:12:42 +0900

    change made in main.cpp in HW6

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -7,9 +7,6 @@ int main(void)
 {
     Stack *stack = new Stack(5); // stack starts
 
-    stack->Push(1); // value to be inserted in stack
-    stack->Push(2); // value to be inserted in stack
-
-    stack->Print(); // printing result but not popping
+    
     return 0;
 }
