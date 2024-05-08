ebbdd69609af21beac6bcba35e27d375e7ef4c2c
hyeokkiyaa <khm38607574@gmail.com>
2024-05-09 03:37:54 +0900

    added comments and Code CLEANUP

--- HW8/BST.h
+++ HW8/BST.h
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
 
-typedef struct node {
+typedef struct node
+{
     int key;
     string name;
     struct node *left_child, *right_child;
 } tree_node;
 
-class BST{
-    private:
-        tree_node* root;
-    public:
-        BST();
-        ~BST();
-        bool IsEmpty();
-        void insert_node(int num,string data);
-        void delete_node(int num);
-        void free_tree(tree_node* tree);
-        void inorder(tree_node* ptr);
-        void Print();
-        tree_node* search_insert(int key);        
+class BST
+{
+private:
+    tree_node *root;
+
+public:
+    BST();                                  // constructor
+    ~BST();                                 // deconstructor
+    bool IsEmpty();                         // if binary tree is empty
+    void insert_node(int num, string data); // insert node
+    void delete_node(int num);              // delelete node
+    void free_tree(tree_node *tree);        // free memory of nodes
+    void inorder(tree_node *ptr);           // inorder system
+    void Print();                           // printout result
+    tree_node *search_insert(int key);      // set if value can be added
 };
--- HW8/main.cpp
+++ HW8/main.cpp
@@ -1,13 +1,16 @@
+// time taken: about 11 hours
 #include <iostream>
 #include <string>
 #include "BST.h"
 using namespace std;
 
+// input function made
 void input(string &line, char &option, int &num, string &name)
 {
     option = line[0];
     switch (option)
     {
+    // case of a will have to obtain two values one for key and one for name in order
     case 'a':
         if (line.length() > 2)
         {
@@ -15,7 +18,7 @@ void input(string &line, char &option, int &num, string &name)
             if (spaceIndex != -1)
             {
                 num = stoi(line.substr(2, spaceIndex - 2));
-                line = line.substr(2,spaceIndex-2);
+                line = line.substr(2, spaceIndex - 2);
                 name = line.substr(spaceIndex + 1);
             }
         }
@@ -26,7 +29,6 @@ void input(string &line, char &option, int &num, string &name)
         break;
     }
 }
-
 
 int main(void)
 {
@@ -41,19 +43,19 @@ int main(void)
         input(line, option, num, name);
         switch (option)
         {
-        case 'q':
+        case 'q': // quit
             break;
             break;
 
-        case 'a':
-            bst.insert_node(num,name);
+        case 'a': // add # name
+            bst.insert_node(num, name);
             break;
 
-        case 'd':
+        case 'd': // del #
             bst.delete_node(num);
             break;
-        
-        case 'p':
+
+        case 'p': // print
             bst.Print();
             break;
         }
