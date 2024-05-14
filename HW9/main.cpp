c8589849d191af9896d34a58c447ca3483109cdc
hyeokkiyaa <khm38607574@gmail.com>
2024-05-14 20:22:15 +0900

    DATAFILE ADDED & FINISHED CODING

--- HW9/main.cpp
+++ HW9/main.cpp
@@ -1,4 +1,3 @@
-// time taken: about 11 hours
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -8,26 +7,94 @@ using namespace std;
 int main(void)
 {
     BST bst;
-    string input;
-    string value;
+    string textFile;
+    string mapFile;
 
-    cin >> input >> value;
-    bst.insert_node(input, value);
-    string token;
-    ifstream file("map.txt");
-    string result="";
-    if(file.is_open()){
-        while(file>>token){
-            if(bst.search(token)!=NULL){
-                result += bst.search(token)->value + " ";
-            }
-            else {
-                result += token +" ";
-            }
+    // Get names of each text files
+    cout << "Type Text File: ";
+    cin >> textFile;
+    cout << "Type Map File: ";
+    cin >> mapFile;
+
+    string line;
+
+    // Get map value and put them in the BST
+    ifstream fileMap(mapFile);
+
+    if (fileMap.is_open())
+    {
+        while (getline(fileMap, line))
+        {
+            string find;
+            int index = line.find(":");
+            find = line.substr(0, index);
+            line = line.substr(index + 2); // since it has : and space
+            bst.insert_node(find, line);   // insert find as key line as valuel
         }
     }
+    else
+    {
+        cout << mapFile << " does not exist" << endl;
+        return 0;
+    }
+
+    fileMap.close();
+
+
+    //find and replace if it exists
+    ifstream fileData(textFile);
+    string token;   //for word
+    string result = ""; //printint out
+    if (fileData.is_open())
+    {
+        while (getline(fileData, line))
+        {
+            //reset token
+            token = "";
+            //length of the line, size_t since c++ suggest size_t for length
+            for (size_t i = 0; i < line.length(); i++)
+            {   
+                //if space is not found
+                if (line[i] != ' ')
+                {
+                    token += line[i];   //make as token
+                }
+                else
+                {
+                    //if space is found then compare the value with key in the bst if do, replace
+                    if (bst.search(token))
+                    {
+                        token = bst.search(token)->value;
+                        result += token + " ";
+                        token = "";
+                    }
+                    else
+                    {
+                        result += token + " ";
+                        token = "";
+                    }
+                }
+            }
+            //for the last one since it does not have space at the end
+            if (bst.search(token))
+            {
+                token = bst.search(token)->value;
+                result += token + " ";
+            }
+            else
+            {
+                result += token + " ";
+            }
+            result += "\n";
+        }
+    }
+    else
+    {
+        cout << textFile << " does not exist" << endl;
+        return 0;
+    }
+    fileData.close();
+
+    //printout result
     cout << result;
-    
-
-
 }
